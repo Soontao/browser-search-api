590a122dbc48b384c03be566dbd1d25d5bb3b128
Theo Sun <theo.sun.github@outlook.com>
2024-05-18 08:39:45 +0800

    chore: update

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,31 @@
-export const a = 1;
+import console from "console";
+import express from "express";
+import playwright from "playwright-core";
+import process from "process";
+import TurndownService from "turndown";
+
+const turndownService = new TurndownService();
+
+if (process.env.PW_REMOTE_URL === undefined) {
+  console.error("PW_REMOTE_URL is required");
+  process.exit(1);
+}
+
+const browser = await playwright.chromium.connect(process.env.PW_REMOTE_URL);
+
+const app = express();
+
+app.get("/sogou", async (req, res) => {
+  const { search } = req.query;
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(`https://www.sogou.com/web?query=${search}`, {
+    waitUntil: "domcontentloaded",
+    timeout: 3000,
+  });
+  return res.end(turndownService.turndown(await page.innerHTML()));
+});
+
+app.listen(parseInt(process.env.PORT) || 3000, () => {
+  console.log("Server is running");
+});
