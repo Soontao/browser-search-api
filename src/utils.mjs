bf66073d63aa3bc88448719650d5105ed8834083
Theo Sun <theo.sun.github@outlook.com>
2024-05-29 22:04:24 +0800

    feat: better timeout handling

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,7 +1,7 @@
+import axios from "axios";
 import console from "console";
 import process from "process";
 import puppeteer from "puppeteer-core";
-import { fetch } from "undici";
 
 /**
  *
@@ -68,18 +68,25 @@ export function createCommonSearchAPI(options) {
           console.warn("TF_URL is not set, skipping text extraction");
           return item;
         }
-        const res = await fetch(process.env.TF_URL + "/extract", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "User-Agent": defaultUserAgent(),
-          },
-          body: JSON.stringify({ url: item.link }),
-        });
-        const data = await res.json();
-        if (res.ok && data.text) {
-          return { ...item, text: data.text };
+        try {
+          const res = await axios.post(
+            process.env.TF_URL + "/extract",
+            { url: item.link },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                "User-Agent": defaultUserAgent(),
+              },
+              timeout: 5_000,
+            },
+          );
+          if (res.status < 300 && res.data?.text) {
+            return { ...item, text: res.data };
+          }
+        } catch (error) {
+          console.error("Failed to extract text", error.message, item.link);
         }
+
         return item;
       }),
     );
