681fe3706a39773296fe8370dedfe0175dcf2972
Theo Sun <theo.sun.github@outlook.com>
2024-05-29 21:16:05 +0800

    feat: with content

--- src/index.mjs
+++ src/index.mjs
@@ -3,7 +3,11 @@ import express from "express";
 import process from "process";
 import puppeteer from "puppeteer-core";
 import { fetch } from "undici";
-import { asyncExpressMiddleware, defaultUserAgent } from "./utils.mjs";
+import {
+  asyncExpressMiddleware,
+  createCommonSearchAPI,
+  defaultUserAgent,
+} from "./utils.mjs";
 
 if (process.env.PW_REMOTE_URL === undefined) {
   console.error("PW_REMOTE_URL is required");
@@ -69,54 +73,12 @@ app.get(
 
 app.get(
   "/bing",
-  asyncExpressMiddleware(async (req, res) => {
-    const { search } = req.query;
-    const browser = await puppeteer.connect({
-      browserWSEndpoint: process.env.PW_REMOTE_URL,
-    });
-    const page = await browser.newPage();
-    await page.setUserAgent(defaultUserAgent());
-    await page.goto(`https://cn.bing.com/search?q=${search}`, {
-      waitUntil: "networkidle0",
-      timeout: 30_000,
-      referer: "https://cn.bing.com/",
-    });
-    const results = await page.$("#b_results");
-    const cards = await results.$$(".b_algo");
-
-    const refLinks = await Promise.all(
-      cards.map(async (card) => {
-        const item = await card.evaluate((node) => {
-          const linkEle = node.querySelector(".b_tpcn a");
-          if (!linkEle) return;
-          const link = linkEle.href;
-
-          // get text
-          const title = linkEle.innerText;
-          const description = node.querySelector(".tptxt")?.innerText;
-          return {
-            title,
-            link,
-            description,
-          };
-        });
-        if (!item?.link) return;
-        const res = await fetch(process.env.TF_URL + "/extract", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "User-Agent": defaultUserAgent(),
-          },
-          body: JSON.stringify({ url: item.link }),
-        });
-        if (!res.ok) return item;
-        const data = await res.json();
-        if (!data.text) return item;
-        return { ...item, text: data.text };
-      }),
-    );
-    await browser.close();
-    return res.json(refLinks.filter(Boolean));
+  createCommonSearchAPI({
+    urlPrefix: "https://cn.bing.com/search?q=",
+    resultsItemSelector: "#b_results .b_algo",
+    titleSelector: ".b_tpcn a",
+    linkSelector: ".b_tpcn a",
+    descriptionSelector: ".tptxt",
   }),
 );
 
--- src/utils.mjs
+++ src/utils.mjs
@@ -1,4 +1,7 @@
 import console from "console";
+import process from "process";
+import puppeteer from "puppeteer-core";
+import { fetch } from "undici";
 
 /**
  *
@@ -12,6 +15,70 @@ export function asyncExpressMiddleware(fn) {
       res.status(500).json({ error: error.message });
     });
   };
+}
+
+/**
+ *
+ * @param {{
+ *  urlPrefix: string
+ *  resultsItemSelector: string,
+ *  titleSelector: string,
+ *  linkSelector: string,
+ *  descriptionSelector: string
+ * }} options
+ * @returns
+ */
+export function createCommonSearchAPI(options) {
+  return asyncExpressMiddleware(async (req, res) => {
+    const { search } = req.query;
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: process.env.PW_REMOTE_URL,
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(defaultUserAgent());
+
+    await page.goto(`${options.urlPrefix}${search}`, {
+      waitUntil: "networkidle0",
+      timeout: 30_000,
+      referer: "https://cn.bing.com/",
+    });
+
+    const cards = await page.$$(options.resultsItemSelector);
+
+    const searchResults = await Promise.all(
+      cards.map(async (card) => {
+        const item = await card.evaluate((node, options) => {
+          const linkEle = node.querySelector(options.linkSelector);
+          const link = linkEle?.href;
+          const title = node.querySelector(options.titleSelector)?.innerText;
+          const description = node.querySelector(
+            options.descriptionSelector,
+          )?.innerText;
+          return {
+            title,
+            link,
+            description,
+          };
+        }, options);
+        if (!item?.link) return;
+        const res = await fetch(process.env.TF_URL + "/extract", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "User-Agent": defaultUserAgent(),
+          },
+          body: JSON.stringify({ url: item.link }),
+        });
+        const data = await res.json();
+        if (res.ok && data.text) {
+          return { ...item, text: data.text };
+        }
+        return item;
+      }),
+    );
+    await browser.close();
+    return res.json(searchResults.filter(Boolean));
+  });
 }
 
 export function defaultUserAgent() {
